56fbe6fcf031e49805d491597eab7f98d49836c1
Nick Zarczynski <jacktradespublic@gmail.com>
2011-11-05 19:25:52 -0500

    Refactored
    drawChart removed from getJSON callback
    getData(symbol) added to wrap getJSON
    event handlers moved out of getJSON callback
    added $(document).ready
    charts can be switched without reseting current day

--- chart.js
+++ chart.js
@@ -31,82 +31,85 @@ function newChart(symbol) {
   var today = 30;
   var chart_length = 15;
 
+  var data = false;
+  getData(symbol);
   
-  
-  function getUrl(symbol) {
-    return "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D'http%3A%2F%2Fichart.finance.yahoo.com%2Ftable.csv%3Fs%3D" + symbol + "%26d%3D2%26e%4D04%26f%3D2011%26g%3Dd%26a%3D0%26b%3D1%26c%3D2000%26ignore%3D.csv'&format=json&callback=?";
-  }
-  
-  $.getJSON(getUrl("ibm"), function (result) {
-    //col0=Date, col1=Open, col2=High, col3=Low, col4=Close, col5=Volume, col6=Adj Close
-    // result_data[0] = headers, result_data[1:] = data, most recent first
-    var result_data = result.query.results.row.slice(1);
-    
-    // Format result_data to change col1->open, col2->high, ...
-    var data = new Array(result_data.length - 1)
-    for (var i=0; i < data.length; i++) {
-      data[i] = {open:  result_data[i].col1,
-              high:  result_data[i].col2,
-              low:   result_data[i].col3,
-              close: result_data[i].col4,
-              date:   result_data[i].col0,
-              volume: result_data[i].col5};
+  $("#next_day").click(function () {
+    if (today > 0) {
+      today--;
+      drawChart();
     }
-    
-    $("#next_day").click(function () {
-      if (today > 0) {
-        today--;
-        drawChart();
-      }
-    });
-    
-    $("#new_symbol").click(function() {
-      newChart("orcl");
-    });
-
-    function drawChart() {
-      c.clearRect(0, 0, width, height);
-      var end = today + chart_length;
-      var low = data[today].low;
-      var high = data[today].high;
-      
-      // get lowest low and highest high
-      for (var i = today; i < end; i++) {
-        if (data[i].low < low) { low = data[i].low }
-        if (data[i].high > high) { high = data[i].high }
-      }
-
-      // get multipliers
-      var height_mul = (height-30) / (high - low);
-      var width_mul = (width-20) / chart_length;
-
-      // draw wicks
-      for (var i = end; i > today; i--) {
-        c.fillStyle = "#000";
-        //c.fillRect((width + ((width / (chart_length * 2)) / 2) - 1) - ((i-today)*width_mul), 
-        c.fillRect(width - ((i-today)*width_mul) + (width_mul / 4) - 1, 
-                  (height-7) - (height_mul * (data[i].low-low)),
-                  2,
-                  (height_mul * (data[i].low - data[i].high)));
-      }
-
-      
-      // draw bodies
-      for (var i = end; i > today; i--) {
-        if (data[i].open < data[i].close) { c.fillStyle = "#00f"; }
-        else { c.fillStyle = "#f00"; }
-        c.fillRect(width - ((i-today)*width_mul), 
-                  (height-7) - (height_mul * (data[i].open-low)),
-                  width / (chart_length*2), //20,
-                  (height_mul * (data[i].open - data[i].close)));
-      }
-    }
-    
-    drawChart();
-    console.log(data.slice(today,today+10));
-
   });
+  $("#new_symbol").click(function() {
+    getData("orcl");
+  });
+
+  
+  function getData(symbol) {
+    var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D'http%3A%2F%2Fichart.finance.yahoo.com%2Ftable.csv%3Fs%3D" + symbol + "%26d%3D2%26e%4D04%26f%3D2011%26g%3Dd%26a%3D0%26b%3D1%26c%3D2000%26ignore%3D.csv'&format=json&callback=?";
+
+    $.getJSON(url, function (result) {
+      //col0=Date, col1=Open, col2=High, col3=Low, col4=Close, col5=Volume, col6=Adj Close
+      // result_data[0] = headers, result_data[1:] = data, most recent first
+      var result_data = result.query.results.row.slice(1);
+      
+      // Format result_data to change col1->open, col2->high, ...
+      data = new Array(result_data.length - 1)
+      for (var i=0; i < data.length; i++) {
+        data[i] = {open:  result_data[i].col1,
+                high:  result_data[i].col2,
+                low:   result_data[i].col3,
+                close: result_data[i].col4,
+                date:   result_data[i].col0,
+                volume: result_data[i].col5};
+      }
+      drawChart();
+      console.log(data.slice(today,today+10));
+    });
+  }
+
+  function drawChart() {
+    c.clearRect(0, 0, width, height);
+    var end = today + chart_length;
+    var low = data[today].low;
+    var high = data[today].high;
+    
+    // get lowest low and highest high
+    for (var i = today; i < end; i++) {
+      if (data[i].low < low) { low = data[i].low }
+      if (data[i].high > high) { high = data[i].high }
+    }
+
+    // get multipliers
+    var height_mul = (height-30) / (high - low);
+    var width_mul = (width-20) / chart_length;
+
+    // draw wicks
+    for (var i = end; i > today; i--) {
+      c.fillStyle = "#000";
+      //c.fillRect((width + ((width / (chart_length * 2)) / 2) - 1) - ((i-today)*width_mul), 
+      c.fillRect(width - ((i-today)*width_mul) + (width_mul / 4) - 1, 
+                (height-7) - (height_mul * (data[i].low-low)),
+                2,
+                (height_mul * (data[i].low - data[i].high)));
+    }
+
+    
+    // draw bodies
+    for (var i = end; i > today; i--) {
+      if (data[i].open < data[i].close) { c.fillStyle = "#00f"; }
+      else { c.fillStyle = "#f00"; }
+      c.fillRect(width - ((i-today)*width_mul), 
+                (height-7) - (height_mul * (data[i].open-low)),
+                width / (chart_length*2), //20,
+                (height_mul * (data[i].open - data[i].close)));
+    }
+  }
+    
 };
 
-newChart("ibm");
+
+$(document).ready(function() {
+  newChart("ibm");
+});
 
