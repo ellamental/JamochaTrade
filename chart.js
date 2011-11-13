22f2c36f4cc52012eb20d2f32de9312e59d5980b
Nick Zarczynski <jacktradespublic@gmail.com>
2011-11-13 07:06:03 -0600

    Pending orders have a cancel button (closes #59)

--- chart.js
+++ chart.js
@@ -152,7 +152,11 @@ function newChart(symbol) {
                 "id": pending_order_counter };
       pending_orders.push(o);
       pending_order_counter++;
-      $("#pending_orders").append('<div id="pending_order_'+o.id+'" class="ui-corner-all ui-widget-content">Limit Buy<br />Symbol: '+symbol+'<br />Shares: '+o.shares+'<br />Price: '+o.price+'</div>');
+      $("#pending_orders").append('<div id="pending_order_'+o.id+'" class="ui-corner-all ui-widget-content">Limit Buy<br />Symbol: '+symbol+'<br />Shares: '+o.shares+'<br />Price: '+o.price+'<br /><button id="cancel_'+o.id+'">Cancel</button></div>');
+      $("#cancel_"+o.id).button();
+      $("#cancel_"+o.id).click(function () {
+        remove_pending_order(o.id);
+      });
       if (pending_orders.length === 1) {
         $("#pending_orders_pane").show();
       }
@@ -165,7 +169,11 @@ function newChart(symbol) {
                 "id": pending_order_counter };
       pending_orders.push(o);
       pending_order_counter++;
-      $("#pending_orders").append('<div id="pending_order_'+o.id+'" class="ui-corner-all ui-widget-content">Stop Buy<br />Symbol: '+symbol+'<br />Shares: '+o.shares+'<br />Price: '+o.price+'</div>');
+      $("#pending_orders").append('<div id="pending_order_'+o.id+'" class="ui-corner-all ui-widget-content">Stop Buy<br />Symbol: '+symbol+'<br />Shares: '+o.shares+'<br />Price: '+o.price+'<br /><button id="cancel_'+o.id+'">Cancel</button></div>');
+      $("#cancel_"+o.id).button();
+      $("#cancel_"+o.id).click(function () {
+        remove_pending_order(o.id);
+      });
       if (pending_orders.length === 1) {
         $("#pending_orders_pane").show();
       }
@@ -233,7 +241,11 @@ function newChart(symbol) {
                   "id": pending_order_counter };
         pending_orders.push(o);
         pending_order_counter++;
-        $("#pending_orders").append('<div id="pending_order_'+o.id+'" class="ui-corner-all ui-widget-content">Limit Sell<br />Symbol: '+sym+'<br />Shares: '+o.shares+'<br />Price: '+o.price+'</div>');
+        $("#pending_orders").append('<div id="pending_order_'+o.id+'" class="ui-corner-all ui-widget-content">Limit Sell<br />Symbol: '+sym+'<br />Shares: '+o.shares+'<br />Price: '+o.price+'<br /><button id="cancel_'+o.id+'">Cancel</button></div>');
+        $("#cancel_"+o.id).button();
+        $("#cancel_"+o.id).click(function () {
+          remove_pending_order(o.id);
+        });
         if (pending_orders.length === 1) {
           $("#pending_orders_pane").show();
         }
@@ -246,7 +258,11 @@ function newChart(symbol) {
                   "id": pending_order_counter };
         pending_orders.push(o);
         pending_order_counter++;
-        $("#pending_orders").append('<div id="pending_order_'+o.id+'" class="ui-corner-all ui-widget-content">Limit Sell<br />Symbol: '+sym+'<br />Shares: '+o.shares+'<br />Price: '+o.price+'</div>');
+        $("#pending_orders").append('<div id="pending_order_'+o.id+'" class="ui-corner-all ui-widget-content">Limit Sell<br />Symbol: '+sym+'<br />Shares: '+o.shares+'<br />Price: '+o.price+'<br /><button id="cancel_'+o.id+'">Cancel</button></div>');
+        $("#cancel_"+o.id).button();
+        $("#cancel_"+o.id).click(function () {
+          remove_pending_order(o.id);
+        });
         if (pending_orders.length === 1) {
           $("#pending_orders_pane").show();
         }
@@ -321,6 +337,18 @@ function newChart(symbol) {
     }
     if (pending_orders.length < 1) {
       $("#pending_orders_pane").hide();
+    }
+  }
+  
+  function remove_pending_order(order_id) {
+    for (var i=0; i < pending_orders.length; i++) {
+      if (pending_orders[i].id === order_id) {
+        pending_orders.splice(i, 1);
+        $("#pending_order_"+order_id).remove();
+        if (pending_orders.length < 1) {
+          $("#pending_orders_pane").hide();
+        }
+      }
     }
   }
   
